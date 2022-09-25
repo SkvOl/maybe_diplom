c271bc1c55616a61058d04efdad18c25a5dd88f2
SkvOl <ghj.ghh.13@mail.ru>
2022-09-25 10:53:47 +0300

    Исправление ошибок. Доработана библиотека матрицы. Добавлен 3-мерный параллельный метод Галёркина.

--- maybe_diplom/matrix.h
+++ maybe_diplom/matrix.h
@@ -7,15 +7,15 @@
 #include <iostream>
 #include <malloc.h>
 #include <fstream>
+#include <complex>
 #include <string>
 #include <math.h>
 #include <mpi.h>
-#include <complex>
 
 using namespace std;
 
 
-template<class type_matrix>
+template<typename type_matrix>
 inline type_matrix** createm(size_t M, size_t N, bool mod = false) {
     //выделение памяти под указатель
     //mod - нужноли при создании создавать единичную матрицу
@@ -35,7 +35,7 @@ inline type_matrix** createm(size_t M, size_t N, bool mod = false) {
     return var;
 }
 
-template<class type_vector>
+template<typename type_vector>
 inline type_vector* createv(size_t N, bool mod = false) {
     //выделение памяти под указатель
     //mod - нужноли при создании создавать единичный вектор
@@ -48,12 +48,19 @@ inline type_vector* createv(size_t N, bool mod = false) {
     return var;
 }
 
-template<class type_matrix_print>
-inline void print(type_matrix_print** var, string c = "") {
+template<typename type_matrix_print>
+inline void print(type_matrix_print** var, string c = "", int cM = -1, int cN = -1) {
     //вывод указателя var в консоль
     //c (color) - цвет главной диагонали при выводе на экран: G, g - зелёный, B, b - синий, R, r - красный, I,i - интенсивнее серого
-    size_t M = _msize(var) / sizeof(var[0]);
-    size_t N = _msize(var[0]) / sizeof(var[0][0]);
+    size_t M, N;
+    if (cM == -1 && cN == -1) {
+        M = _msize(var) / sizeof(var[0]);
+        N = _msize(var[0]) / sizeof(var[0][0]);
+    }
+    else {
+        M = cM;
+        N = cN;
+    }
 
     const char* type = "";
     if (sizeof(type_matrix_print) == sizeof(int))  type = "%d ";
@@ -92,7 +99,7 @@ inline void print(type_matrix_print** var, string c = "") {
     fflush(stdout);
 }
 
-template<class type_vector_print>
+template<typename type_vector_print>
 inline void print(type_vector_print* var) {
     //вывод указателя var в консоль
     size_t M = _msize(var) / sizeof(var[0]);
@@ -101,11 +108,12 @@ inline void print(type_vector_print* var) {
     if (sizeof(type_vector_print) == sizeof(int))  type = "%d\n";
     if (sizeof(type_vector_print) == sizeof(double))  type = "%f\n";
     if (sizeof(type_vector_print) == sizeof(complex<double>))  type = "complex";
-    
+
     for (size_t i = 0; i < M; i++)
     {
         if (type != "complex") printf(type, var[i]);
         else cout << var[i] << "\n";
+
         fflush(stdout);
     }
     printf("\n");
@@ -121,8 +129,8 @@ void space(size_t k = 0) {
     }
 }
 
-template<class type_matrix_size>
-int size(type_matrix_size**var) {
+template<typename type_matrix_size>
+int size(type_matrix_size** var) {
     //вычисляет объём занимаемой памяти указателем var
     size_t M = _msize(var) / sizeof(var[0]);
     size_t sum = 0;
@@ -131,7 +139,7 @@ int size(type_matrix_size**var) {
     return sum;
 }
 
-template<class type_vector_size>
+template<typename type_vector_size>
 int size(type_vector_size* var) {
     //вычисляет объём занимаемой памяти указателем var
     return _msize(var);
@@ -247,19 +255,215 @@ string gm(type_gm**& var, int* count_one_rank = NULL, int _step = 0, int _rank =
     return "rank: " + to_string(_rank) + "  Successfully";
 }
 
-template<class type_matrix_del>
+template<typename type_fill>
+type_fill* fill(int M, type_fill item = 0.0) {
+    type_fill* var = createv<type_fill>(M);
+    for (size_t i = 0; i < M; i++) {
+        var[i] = item;
+    }
+    return var;
+}
+
+template<typename type_simple_iteration>
+type_simple_iteration* simple_iteration(type_simple_iteration** var = 0, type_simple_iteration* init_approx = 0, size_t k = 0)
+{
+    size_t M = _msize(var) / sizeof(var[0]);
+    printf("simple iteration M: %d\n", M);
+
+    type_simple_iteration* res = createv<type_simple_iteration>(M);
+    size_t k_ind = 0;
+    while (true) {
+        res = fill<type_simple_iteration>(M);
+        k_ind++;
+        for (size_t i = 0; i < M; i++)
+        {
+            if (i % 100 == 0 && i != 0)
+            {
+                printf("simple iteration i=%d\n", i);
+            }
+            if (var[i][i] > 0) {
+                for (size_t j = 0; j < M; j++)
+                {
+                    if (i == j)res[i] -= (var[i][j] - 1) * init_approx[j];
+                    if (i != j)res[i] -= var[i][j] * init_approx[j];
+                }
+                res[i] += var[i][M];
+            }
+            if (var[i][i] < 0) {
+                for (size_t j = 0; j < M; j++)
+                {
+                    if (i == j) res[i] += (var[i][j] + 1) * init_approx[j];
+                    if (i != j) res[i] += var[i][j] * init_approx[j];
+                }
+                res[i] -= var[i][M];
+            }
+        }
+        if (k_ind <= k) {
+            init_approx = res;
+            res = fill<type_simple_iteration>(M);
+        }
+        else {
+            return res;
+        }
+    }
+}
+
+complex<double>* simple_iteration_c(complex<double>** var = 0, complex<double>* init_approx = 0, size_t k = 0)
+{
+    size_t M = _msize(var) / sizeof(var[0]);
+    complex<double>* res = createv<complex<double>>(M), one = 1.0;
+    size_t k_ind = 0;
+    while (true) {
+        res = fill<complex<double>>(M);
+        k_ind++;
+        for (size_t i = 0; i < M; i++)
+        {
+            if (var[i][i].real() > 0) {
+                for (size_t j = 0; j < M; j++)
+                {
+                    if (i == j)res[i] -= (var[i][j] - one) * init_approx[j];
+                    if (i != j)res[i] -= var[i][j] * init_approx[j];
+                }
+                res[i] += var[i][M];
+            }
+            if (var[i][i].real() < 0) {
+                for (size_t j = 0; j < M; j++)
+                {
+                    if (i == j) res[i] += (var[i][j] + one) * init_approx[j];
+                    if (i != j) res[i] += var[i][j] * init_approx[j];
+                }
+                res[i] -= var[i][M];
+            }
+        }
+        if (k_ind <= k) {
+            init_approx = res;
+            res = fill<complex<double>>(M);
+        }
+        else {
+            return res;
+        }
+    }
+}
+
+template<typename type_matrix_del>
 void del(type_matrix_del**& var) {
     //очистка памяти указателя var
     size_t M = _msize(var) / sizeof(var[0]);
-    
+
     for (size_t i = 0; i < M; i++) free(var[i]);
     free(var);
 }
 
-template<class type_vector_del>
+template<typename type_vector_del>
 void del(double*& var) {
     //очистка памяти указателя var
     free(var);
+}
+
+template<typename type_LU>
+void LU(type_LU** var, type_LU**& L, type_LU**& U) {
+    size_t M = _msize(var) / sizeof(var[0]);
+
+    for (size_t i = 0; i < M; i++) {
+        for (size_t j = 0; j < M; j++) {
+            if (i == j) L[i][i] = 1;
+            else L[i][j] = 0;
+            U[i][j] = 0;
+        }
+    }
+    for (short i = 0; i < M; i++) {
+        for (short j = 0; j < M; j++) {
+            type_LU sumU = 0, sumL = 0;
+            if (i <= j) {
+                for (short z = 0; z <= i - 1; z++) {
+                    sumU += L[i][z] * U[z][j];
+                }
+                U[i][j] = var[i][j] - sumU;
+            }
+
+            if (i > j) {
+                for (short z = 0; z <= j - 1; z++) {
+                    sumL += L[i][z] * U[z][j];
+                }
+                L[i][j] = (var[i][j] - sumL) / U[j][j];
+            }
+        }
+    }
+}
+
+template<typename type_mult>
+inline type_mult** mult(type_mult** var1, type_mult** var2) {
+    size_t M = _msize(var1) / sizeof(var1[0]);
+    size_t N = _msize(var1[0]) / sizeof(var1[0][0]);
+
+    type_mult** res = createm<type_mult>(M, N);
+    for (size_t i = 0; i < M; i++)
+    {
+        for (size_t j = 0; j < M; j++)
+        {
+            type_mult sum = 0;
+            for (size_t k = 0; k < M; k++)
+            {
+                sum += var1[i][k] * var2[k][j];
+            }
+            res[i][j] = sum;
+        }
+    }
+    return res;
+}
+
+template<typename type_diag>
+type_diag** diag(type_diag** var) {
+    size_t M = _msize(var) / sizeof(var[0]);
+
+
+    type_diag** var_D = createm<type_diag>(M, M), ** var_L = createm<type_diag>(M, M), ** var_U = createm<type_diag>(M, M);
+
+    LU(var, var_L, var_U);
+    var_D = mult(var_U, var_L);
+
+    for (size_t k = 1; k <= 10; k++) {
+        LU(var_D, var_L, var_U);
+        var_D = mult(var_U, var_L);
+    }
+    //free(var_U); free(var_L);
+
+    for (size_t i = 0; i < M; i++)
+    {
+        for (size_t j = 0; j < M; j++)
+        {
+            if (i != j) var_D[i][j] = 0;
+        }
+    }
+    return var_D;
+}
+
+template<typename type_eigenvalues>
+type_eigenvalues* eigenvalues(type_eigenvalues** var) {
+    size_t M = _msize(var) / sizeof(var[0]);
+
+    type_eigenvalues** var_D = diag<type_eigenvalues>(var), * res = createv<type_eigenvalues>(M);
+    for (size_t i = 0; i < M; i++)
+        res[i] = var_D[i][i];
+
+    return res;
+}
+
+template<typename type_cond>
+type_cond cond(type_cond** var) {
+    size_t M = _msize(var) / sizeof(var[0]);
+
+
+    type_cond* var1 = eigenvalues(var);
+
+    type_cond max = 0, min = 0;
+    for (size_t i = 0; i < M; i++)
+    {
+        if (abs(var1[i]) >= max) max = abs(var1[i]);
+        if (abs(var1[i]) <= max) min = abs(var1[i]);
+    }
+
+    return max / min;
 }
 
 #endif MATRIX_H
--- maybe_diplom/maybe_diplom.cpp
+++ maybe_diplom/maybe_diplom.cpp
@@ -2,23 +2,20 @@
 #include "integrals.h"
 
 
-const double pi = 3.1415926, Eps = 0.0001;
-const double  k0 = 1, k1 = 1.5 * k0;
 
-const int _n = 5, _N = _n * _n;
+int _n = 2, _N = _n * _n;
 
-double R = 5;
-double lambda = 1;
 
-//отрезок для двумерных интегральных уравнений
+//отрезок для n мерных интегральных уравнений
 double A = 0, B = 1;
 double C = 0, D = 1;
+double E = 0, F = 1;
 
-//шаг для одномерных интегральных уравнений
-double h = (B - A) / _n;
 
-//отрезок для двумерных интегральных уравнений
-double h1 = (B - A) / _n, h2 = (D - C) / _n;
+//шаг  для n мерных интегральных уравнений
+double h1 = (B - A) / _n;
+double h2 = (D - C) / _n;
+double h3 = (F - E) / _n;
 
 
 inline double u(double y1) {     
@@ -35,10 +32,10 @@ void mk(double**& var, size_t type, size_t dim_s = 1) {
 
     if(dim_s == 1)
         for (size_t i = 0; i < M; i++){
-            double ksi = A + (i + 0.5) * h;
+            double ksi = A + (i + 0.5) * h1;
 
             for (size_t j = 0; j < M; j++){
-                double a = A + j * h, b = A + (j + 1.0) * h;
+                double a = A + j * h1, b = A + (j + 1.0) * h1;
                 
                 var[i][j] = base_func(i, j) * (type - 1.0) - lambda * I_k(100, a, b, ksi);
             }
@@ -67,40 +64,62 @@ void mg(double**& var, size_t type, size_t dim_s = 1, int _step = 0) {
     size_t M = _msize(var) / sizeof(var[0]);
     size_t N = _msize(var[0]) / sizeof(var[0][0]);
 
-    int m = (int)sqrt(N - 1);
-
     if (dim_s == 1) {
         for (size_t i = 0; i < M; i++) {
-            double a = A + (i + _step) * h, b = A + ((i + _step) + 1.0) * h;
+            double a = A + (i + _step) * h1, b = A + ((i + _step) + 1.0) * h1;
 
-            for (size_t j = 0; j < N - 1; j++) {
-                double c = A + j * h, d = A + (j + 1.0) * h;
+            for (size_t j = 0; j < N; j++) {
+                double c = A + j * h1, d = A + (j + 1.0) * h1;
 
-                var[i][j] = h * base_func(i + _step, j) * (type - 1.0) - lambda * I(100, k, a, b, c, d);
+                var[i][j] = h1 * base_func((i + _step), j) * (type - 1.0) - lambda * In<double>(3, k, a, b, c, d);
             }
-            var[i][N - 1] = I(100, a, b);
+            var[i][N - 1] = In<double>(3, a, b);
         }
     }
-    else {
-        for (size_t j = 0; j < M; j++) {
-            short j1 = (j + _step) / m, j2 = (j + _step) % m;
-            double e = A + j1 * h1, f = A + (j1 + 1.0) * h1;
-            double g = C + j2 * h2, l = C + (j2 + 1.0) * h2;
+    else if (dim_s == 2) {
+        int m = (int)sqrt(N - 1);
+        for (size_t i = 0; i < M; i++) {
+            short i1 = (i + _step) / m, i2 = (i + _step) % m;
+            double a = A + i1 * h1, b = A + (i1 + 1.0) * h1;
+            double c = C + i2 * h2, d = C + (i2 + 1.0) * h2;
 
-            for (size_t i = 0; i < N - 1; i++) {
-                short i1 = i / m, i2 = i % m;
-                double a = A + i1 * h1, b = A + (i1 + 1.0) * h1;
-                double c = C + i2 * h2, d = C + (i2 + 1.0) * h2;
+            //printf("i=%d\n", i);
+            for (size_t j = 0; j < N; j++) {
+                short j1 = j / m, j2 = j % m;
+                double e = A + j1 * h1, f = A + (j1 + 1.0) * h1;
+                double g = C + j2 * h2, l = C + (j2 + 1.0) * h2;
 
-                var[j][i] = h1 * h2 * base_func(i, j + _step) * (type - 1.0) - lambda * I(10, k, a, b, c, d, e, f, g, l);
+                var[i][j] = h1 * h2 * base_func(i + _step, j) * (type - 1.0) - lambda * In<double>(3, k, a, b, c, d, e, f, g, l);
             }
-            var[j][N - 1] = I(100, func, e, f, g, l);
+            var[i][N - 1] = In<double>(3, func, a, b, c, d);
+        }
+    }
+    else if (dim_s == 3) {
+        int m = (int)pow(N - 1, 1.0 / 3.0);
+
+        for (size_t i = 0; i < M; i++) {
+            short i1 = (i + _step) % m, i2 = (i + _step) / m - m * ((i + _step) / m / m), i3 = (i + _step) / m / m;
+            double a = A + i1 * h1, b = a + h1;
+            double c = C + i2 * h2, d = c + h2;
+            double e = E + i3 * h3, f = e + h3;
+        
+            for (size_t j = 0; j < N; j++) {
+                short j1 = j % m, j2 = j / m - m * (j / m / m), j3 = j / m / m;
+
+                double g = A + j1 * h1, l = g + h1;
+                double o = C + j2 * h2, p = o + h2;
+                double q = E + j3 * h3, s = q + h3;
+
+                var[i][j] = h1 * h2 * h3 * base_func(i + _step, j) * (type - 1.0) - lambda * In<double>(5, k, a, b, c, d, e, f, g, l, o, p, q, s);                       
+            }
+            var[i][N - 1] = In<double>(5, func, a, b, c, d, e, f);
         }
     }
 }
 
 
-int main() {
+int main1() {
+    return 0;
     int _rank, _size;
     
     MPI_Init(NULL, NULL);
@@ -135,7 +154,7 @@ int main() {
     double t2 = MPI_Wtime() - t1;
     printf("rank: %d  time fill matrix is: %f\n", _rank, t2);
     fflush(stdout);
-    //print(a);
+    //if (_rank == 0)print(a);
 
     t1 = MPI_Wtime();
     gm(a, count_one_rank, _step, _rank, _size);
@@ -178,3 +197,81 @@ int main() {
     MPI_Finalize();
 }
 
+int main() {
+    //return 0;
+    _N *= _n;
+    int _rank, _size;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &_rank);
+    MPI_Status status;
+    MPI_Request request;
+
+    /// подготовительные работы
+    int* count_one_rank = createv<int>(_size);
+
+    for (size_t i = 0; i < _size; i++)
+        count_one_rank[i] = _N / _size;
+
+    short mod = _N % _size;
+    if (mod != 0)
+        for (short i = _size - 1; i >= 0; i--) {
+            count_one_rank[i]++;
+            mod--;
+            if (mod == 0) break;
+        }
+
+    int _step = 0;
+    for (size_t i = 0; i < _rank; i++)
+        _step += count_one_rank[i];
+    /// подготовительные работы
+
+    double** a = createm<double>(count_one_rank[_rank], _N + 1);
+
+    double t1 = MPI_Wtime();
+    mg(a, 2, 3, _step);
+    double t2 = MPI_Wtime() - t1;
+    printf("rank: %d  time fill matrix is: %f\n", _rank, t2);
+    fflush(stdout);
+    //if (_rank == 0) print(a);
+
+    t1 = MPI_Wtime();
+    gm(a, count_one_rank, _step, _rank, _size);
+    t2 = MPI_Wtime() - t1;
+    printf("rank: %d  time Gauss method is: %f\n", _rank, t2);
+    fflush(stdout);
+
+
+    double* part_res = createv<double>(count_one_rank[_rank]), * res = NULL;
+    int* arr_step = NULL;
+
+
+    for (size_t i = 0; i < count_one_rank[_rank]; i++)
+        part_res[i] = a[i][_N];
+
+
+    if (_rank == 0) {
+        del(a);
+
+        res = createv<double>(_N);
+        arr_step = createv<int>(_size);
+
+        for (size_t i = 0; i < _size; i++) {
+            arr_step[i] = 0;
+
+            for (size_t j = 0; j < i; j++)
+                arr_step[i] += count_one_rank[j];
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Gatherv(part_res, count_one_rank[_rank], MPI_DOUBLE, res, count_one_rank, arr_step, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    if (_rank == 0)
+        for (size_t i = 0; i < _N; i++) {
+            printf("%f\n", res[i]);
+        }
+
+
+    MPI_Finalize();
+}
